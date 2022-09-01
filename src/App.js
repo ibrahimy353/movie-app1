c76f42472ad155c8ce371375d763425481b2649e
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-09-01 23:13:09 +0300

    Added to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import MovieBox from './MovieBox';
 
+const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=d00edc8e486cd903348d22749de24834";
 function App() {
+  
+  const[movies,setMovies]=useState([]);
+  
+  useEffect (()=> {
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then(data=>{
+      console.log(data)
+      setMovies(data.results);
+    })
+
+  }, [])
+
+
+
   return (
     <div>
-      movie app
+      {movies.map((movieReq)=>
+      <MovieBox key={movieReq.id} {...movieReq}/>)}
      
     </div>
   );
