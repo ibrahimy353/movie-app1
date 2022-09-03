6e0b97a3c9d62cc463ee5e9dacaafad2daac327c
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-09-03 23:40:08 +0300

    added to project

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,14 +1,21 @@
 import { Modal } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 
 const API_IMG="https://image.tmdb.org/t/p/w500"
 const MovieBox =({title, poster_path, vote_avarage,release_date, overview})=>{
+
+    const [show, setShow] =useState(false);
+    const handleShow()=>setShow(true);
+    const handleShow=()=>setShow(false);
+
+
     return(
         <div className="card text-center bg-secondary mb-3">
             <div className="card-body">
                 <img className="card-img-top" src={API_IMG+poster_path}/>
                 <div className="card-body">
-                    <button type="button" className="btn btn-dark">View More</button>
+                    <button type="button" className="btn btn-dark" onClick={handleShow}
+                    >View More</button>
 
                     <Modal>
                         <Modal.Header closeButton>
