d83a0e8a49943c93d73c4859ed601d1c5dc7785c
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-09-03 23:02:57 +0300

    added to project

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -6,16 +6,25 @@ const MovieBox =({title, poster_path, vote_avarage,release_date, overview})=>{
     return(
         <div className="card text-center bg-secondary mb-3">
             <div className="card-body">
-                <img className="card-img-top" src={API_IMG=poster_path}/>
+                <img className="card-img-top" src={API_IMG+poster_path}/>
                 <div className="card-body">
                     <button type="button" className="btn btn-dark">View More</button>
 
                     <Modal>
                         <Modal.Header closeButton>
                             <Modal.Title>
-                                
+
                             </Modal.Title>
                         </Modal.Header>
+                        <Modal.Body>
+                            <img className="card-img-top" src={API_IMG+poster_path}></img>
+                            <h3>{title}</h3>
+                            <h4>ImDb:</h4>
+                            <h5>Release Date</h5>
+                            <br></br>
+                            <h6>Overview</h6>
+
+                        </Modal.Body>
 
                     </Modal>
                 
