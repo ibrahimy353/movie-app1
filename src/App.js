3c325e0f3857c0d173ab6beac81f4da47e93abfb
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-09-05 21:42:16 +0300

    added to app

--- src/App.js
+++ src/App.js
@@ -1,72 +1,86 @@
-import React,{useEffect, useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
-import  'bootstrap/dist/css/bootstrap.min.css';
-import { Container, FormControl, Nav, Navbar, Button } from 'react-bootstrap';
-import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
-import  Button  from 'react-bootstrap/esm/Button';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar,Container,Nav,Form, FormControl,Button } from 'react-bootstrap';
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=d00edc8e486cd903348d22749de24834";
+const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=d00edc8e486cd903348d22749de24834&query";
+
 function App() {
-  
-  const[movies,setMovies]=useState([]);
-  
-  useEffect (()=> {
+
+  const [movies, setMovies]=useState([]);
+  const [query, setQuery]=useState('');
+
+  useEffect(() => {
     fetch(API_URL)
     .then((res)=>res.json())
-    .then(data=>{ 
-      console.log(data)
+    .then(data=>{
+      console.log(data);
       setMovies(data.results);
     })
-
   }, [])
 
 
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try{
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+      const res= await fetch(url);
+      const data= await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
 
+  const changeHandler=(e)=>{
+    setQuery(e.target.value);
+  }
   return (
     <>
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container fluid>
-        <Navbar.Brand href="">Ibra'S MovieDb App</Navbar.Brand>
-         <Navbar.Brand href="">Trending</Navbar.Brand>
-         <Navbar.Brand href="">Latest</Navbar.Brand>
-          <NavbarToggle aria-controls="navbarscroll">
-          
-            <NavbarCollapse id="navbarScroll">
-              <Nav
-               className="me-auto my-2 my-lg-3"
-               style={{maxHeight:'100px'}}
-               navbarScroll>
-              </Nav>
+        <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
+        <Navbar.Brand href="/home">Trending</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
 
-              <form className="d-flex">
+          <Navbar.Collapse id="nabarScroll">
+            <Nav 
+            className="me-auto my-2 my-lg-3"
+            style={{maxHeight:'100px'}}
+            navbarScroll></Nav>
 
-                <FormControl
-                type="search"
-                placeholder="Movie-Search"
-                className='me-2'
-                aria-lable="search"
-                name=""></FormControl>
-                <Button variant="secondary" type="submit">Search</Button>
-              </form>
-
-            </NavbarCollapse>
-          </NavbarToggle>
+            <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
+              type="search"
+              placeholder="Movie Search"
+              className="me-2"
+              aria-label="search"
+              name="query"
+              value={query} onChange={changeHandler}></FormControl>
+              <Button variant="secondary" type="submit">Search</Button>
+            </Form>
+          </Navbar.Collapse>
       </Container>
-
     </Navbar>
-     
-     
-    <div className='container'>
-      <div className='grid'>
-        {movies.map((movieReq)=>
-        <MovieBox key={movieReq.id} {...movieReq}/>)}
-      </div>
+    <div>
+      {movies.length > 0 ?(
+        <div className="container">
+        <div className="grid">
+          {movies.map((movieReq)=>
+          <MovieBox key={movieReq.id} {...movieReq}/>)}
+            </div>
     </div>
-    
-  </>
+      ):(
+        <h2>Sorry !! No Movies Found</h2>
+      )}
+    </div>   
+    </>
+   
   );
 }
 
