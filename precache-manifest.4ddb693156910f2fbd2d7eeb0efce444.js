7029ae09395d7c1d6244715dc96508dd7c2a338d
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-09-07 14:17:39 +0300

    Updates

---
+++ precache-manifest.4ddb693156910f2fbd2d7eeb0efce444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1310964b65c2f7493c27",
+    "url": "/static/css/main.a870a0f1.chunk.css"
+  },
+  {
+    "revision": "1310964b65c2f7493c27",
+    "url": "/static/js/main.1310964b.chunk.js"
+  },
+  {
+    "revision": "881479e9c8366c5be5ba",
+    "url": "/static/css/1.743917b1.chunk.css"
+  },
+  {
+    "revision": "881479e9c8366c5be5ba",
+    "url": "/static/js/1.881479e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "533e4cd671398be869a225b4f9c37835",
+    "url": "/index.html"
+  }
+];
